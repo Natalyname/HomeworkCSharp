a5afbd4b8d8b9d42de22fb8e16df6db3810aae1d
Nata <lalliana@list.ru>
2022-12-21 00:10:11 +0300

    Задача № 15 решена

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите цифру, обозначающую день недели: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine(a > 0 && a < 6 ? "нет" : (a==6 || a ==7) ? "да" : "Введено неверное число");
