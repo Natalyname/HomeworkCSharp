970a73f6fcc3cb65854fdfbd92b1a8124da8d9e1
Nata <lalliana@list.ru>
2022-12-20 23:58:15 +0300

    Задача № 13 решена

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int num = int.Parse(ReadLine()!);
+
+if (num > 99)
+    {
+    while (num/100 > 10)
+    {
+    num = num/10;
+    }
+    WriteLine($"Третья цифра - {num%10}");
+    }
+
+else
+{
+    WriteLine("Нет третьей цифры");
+}
