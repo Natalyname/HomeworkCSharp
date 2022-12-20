f78e031cbf72f7990ce8fb23f7bc46a64b52cb34
Nata <lalliana@list.ru>
2022-12-20 22:56:44 +0300

    Task 10 solved

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число от 100 до 999: ");
+int a = int.Parse(ReadLine()!);
+int b = (a % 100);
+
+WriteLine($"Вторая цифра числа {a} равна {b / 10}");
