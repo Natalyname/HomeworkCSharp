68a781c6d70cb066922d543ac8caaead8832c9ef
Nata <lalliana@list.ru>
2022-12-25 23:54:10 +0300

    Task № 19 added

--- Homework3/1/Program.cs
+++ Homework3/1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите пятизначное число: ");
+int n = int.Parse(ReadLine()!);
+
+WriteLine((n > 99999 || n < 10000)? "Число не пятизначное" : n%10==n/10000 && n/1000%10==n/10%10 ? "Число является палиндромом" : "Число не является палиндромом");
