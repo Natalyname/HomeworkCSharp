9b5c7ef0b2355ed7f90072c355373e71ad225c2a
Nata <lalliana@list.ru>
2022-12-26 13:52:31 +0300

    Task № 21 added

--- Homework3/2/Program.cs
+++ Homework3/2/Program.cs
@@ -1,1 +1,21 @@
 ﻿using static System.Console;
+Clear();
+
+WriteLine("Введиты координвты точки А: ");
+Write("x = ");
+int.TryParse(ReadLine(), out int ax);
+Write("y = ");
+int.TryParse(ReadLine(), out int ay);
+Write("z = ");
+int.TryParse(ReadLine(), out int az);
+WriteLine("Введите координаты точки B: ");
+Write("x = ");
+int.TryParse(ReadLine(), out int bx);
+Write("y = ");
+int.TryParse(ReadLine(), out int by);
+Write("z = ");
+int.TryParse(ReadLine(), out int bz);
+
+double s=Math.Sqrt(((bx-ax)*(bx-ax))+((by-ay)*(by-ay))+((bz-az)*(bz-az)));
+WriteLine($"{s:f2}");
+
