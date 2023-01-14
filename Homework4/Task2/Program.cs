9620dece96aa3cb517ed58c94e20ce4be2357100
Nataly <lalliana@list.ru>
2023-01-14 15:21:55 +0300

    Добавлена задача на сумму цифр в числе

--- Homework4/Task2/Program.cs
+++ Homework4/Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, реализующую метод, который принимает на вход число и выдаёт 
+//сумму цифр в числе.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int Sum = GetSum(number);
+WriteLine($"Сумма цифр в числe {number} - {Sum}");
+
+int GetSum(int A)
+
+ {
+    int result = 0;
+    result += A % 10;
+    A /= 10;
+        if(A > 0)
+        {
+        result += GetSum(A);
+        }
+        return result;
+}
