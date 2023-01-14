a21f131a4cde62977c264da7b6ab62a522c428da
Nataly <lalliana@list.ru>
2023-01-14 15:17:37 +0300

    Добавлена задача на квадрат числа

---
+++ Homework4/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, реализующую метод,который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B с использованием цикла.
+
+using static System.Console;
+Clear();
+
+Write("Введите первое число: ");
+int number1 = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int number2 = int.Parse(ReadLine()!);
+int pow = GetPow (number1, number2);
+WriteLine($"Квадрат числа {number1} равен {pow}");
+
+
+int GetPow (int X, int N)
+{
+int result = 1;
+int i = 1;
+while (i<=N)
+{
+   result *=X;
+    i++;
+}
+
+return result;
+}
