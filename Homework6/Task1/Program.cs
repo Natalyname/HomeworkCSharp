77006e7ba2d68faed67060b7f7dd59dea271a906
Nataly <lalliana@list.ru>
2023-01-20 21:09:29 +0300

    Задача 41 решена

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+//больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+
+using static System.Console;
+Clear();
+
+Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+int count = 0; 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+WriteLine($"Кол-во чисел больше нуля: {count}");
